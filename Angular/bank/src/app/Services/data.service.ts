470006ad082c361ad82b529e259f339ae064fdf0
Basil T Joy <basiltjoy96@gmail.com>
2021-11-23 23:43:01 +0530

    initiate

--- Angular/bank/src/app/Services/data.service.ts
+++ Angular/bank/src/app/Services/data.service.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -39,12 +40,53 @@ export class DataService {
       else {
         alert("Invalid password")
         return false
-      }  
+      }
     }
     else {
       alert("Invalid Account number")
       return false
     }
   }
+
+  credit(accno: any, pwd: any, amt: any) {
+    var amount = parseInt(amt)
+    let database = this.data
+    if (accno in database) {
+      if (pwd == database[accno].password) {
+        database[accno].balance = database[accno].balance + amount
+        return database[accno].balance
+      }
+      else {
+        alert("Invalid Password")
+      }
+    }
+    else {
+      alert("Invalid Account number")
+      return false
+    }
+  }
+
+  debit(accno1: any, pwd1: any, amt1: any) {
+    var amount = parseInt(amt1)
+    let database = this.data
+    if (accno1 in database) {
+      if (pwd1 == database[accno1].password) {
+        if (database[accno1].balance > amt1) {
+          database[accno1].balance = database[accno1].balance - amount
+          return database[accno1].balance
+        }
+        else{
+          alert("Insufficient Fund")
+        }
+      }
+      else {
+        alert("invalid password")
+      }
+    }
+    else {
+      alert("Invalid Account number")
+    }
+  }
+
 }
 
--- Angular/bank/src/app/dashboard/dashboard.component.ts
+++ Angular/bank/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../Services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,17 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  accno = ""
+  pwd = ""
+  amt = ""
 
-  constructor() { }
+  accno1 = ""
+  pwd1 = ""
+  amt1 = ""
+
+  constructor(private router: Router, private ds: DataService) { }
 
   ngOnInit(): void {
   }
 
   credit() {
-    alert("Are you sure to proceed with transaction")
+    var accno = this.accno
+    var pwd = this.pwd
+    var amt = this.amt
+    var result = this.ds.credit(accno, pwd, amt)
+    if (result) {
+      alert(amt+"rs  Credited successfully, Avail balance :"+ result)
+
+    }
   }
 
   debit() {
-    alert("Are you sure to proceed with transaction")
+    var accno1 = this.accno1
+    var pwd1 = this.pwd1
+    var amt1 = this.amt1
+    var result = this.ds.debit(accno1, pwd1, amt1)
+    if(result){
+      alert(amt1+"rs  Debited successfully, Avail balance :"+ result)
+    }
+  }
+
+  logout() {
+    alert("Are you sure you want to exit")
+    this.router.navigateByUrl('')
   }
 }
